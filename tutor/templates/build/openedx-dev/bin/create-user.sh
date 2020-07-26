d955a5fe4799f1216a9e24c46ad225ee8188daa5
Régis Behmo <regis@behmo.com>
2020-07-26 13:32:00 +0200

    WIP attempt to faster chown /openedx folder in openedx-dev image
    This is for #323.

--- tutor/templates/build/openedx-dev/bin/create-user.sh
+++ tutor/templates/build/openedx-dev/bin/create-user.sh
@@ -5,7 +5,6 @@ if [ "$USERID" != "" ] && [ "$USERID" != "0" ]
 then
   echo "Creating 'openedx' user with id $USERID"
   useradd --home-dir /openedx --uid $USERID openedx
-  chown -R openedx:openedx /openedx
 else
   echo "Running as root"
 fi
